c09959da977c4ee114ac92d61f1a5a6129b6e278
saaketh <saakethram143chotu@gmail.com>
2025-06-10 10:53:41 +0530

    minor adjustments

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,12 +1,4 @@
 from fastapi import APIRouter
 
-
-
-from fastapi import FastAPI, Request, Response, Depends, HTTPException, status, Cookie
-from datetime import datetime, timedelta
-#import redis
-import uuid
-import json
-import os
-
 router=APIRouter()
+
