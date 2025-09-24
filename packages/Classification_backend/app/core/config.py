eacbe7d5de4e036ef52a16ebbef54588e4cb698c
Jagdeep Singh <jagdeepsingh1630@gmail.com>
2025-09-24 13:44:50 +0530

    policy file path

--- packages/Classification_backend/app/core/config.py
+++ packages/Classification_backend/app/core/config.py
@@ -13,7 +13,6 @@ class Settings(BaseSettings):
     GOOGLE_CLOUD_LOCATION: str = ""
     GOOGLE_APPLICATION_CREDENTIALS: str = ""
     OCR_TIMEOUT: int = 5
-    POLICY_FILE_PATH: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
